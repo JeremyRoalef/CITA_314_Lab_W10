c7be3e70a9ff7fda28a826a424bbd098be4d0c79
JeremyRoalef <roalef944@morrisville.edu>
2025-04-02 13:12:16 -0400

    Added grab interactable sound effects

--- VRCourse/Assets/Scripts/System/XRAudioManager.cs
+++ VRCourse/Assets/Scripts/System/XRAudioManager.cs
@@ -2,9 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRAudioManager : MonoBehaviour
 {
+    [SerializeField]
+    XRGrabInteractable[] grabInteractables;
+
+    [SerializeField]
+    AudioSource grabSound;
+
+    [SerializeField]
+    AudioClip grabClip;
+
+    [SerializeField]
+    AudioClip keyClip;
+
+    [SerializeField]
+    AudioSource activatedSound;
+
+    [SerializeField]
+    AudioClip grabActivatedClip;
+
+    [SerializeField] 
+    AudioClip wandActivatedClip;
+
     [SerializeField]
     TheWall wall;
 
@@ -21,13 +43,22 @@ public class XRAudioManager : MonoBehaviour
 
     private void OnEnable()
     {
-        //
+        grabInteractables = FindObjectsByType<XRGrabInteractable>(FindObjectsSortMode.None);
+        for (int i = 0; i < grabInteractables.Length; i++)
+        {
+            grabInteractables[i].selectEntered.AddListener(OnSelectEnteredGrabbable);
+            grabInteractables[i].selectExited.AddListener(OnSelectExitedGrabbable);
+            grabInteractables[i].activated.AddListener(OnActivatedGrabbable);
+        }
+
+
+        //Check if there's a fallback clip
         if (fallBackClip == null)
         {
             fallBackClip = AudioClip.Create(FALL_BACK_CLIP, 1, 1, 1000, true);
         }
 
-        //
+        //Check if wall exists
         if (wall != null)
         {
             destoryWallClip = wall.GetDestroyClip;
@@ -39,15 +70,6 @@ public class XRAudioManager : MonoBehaviour
         }
     }
 
-    private void OnDestroyWall()
-    {
-        //Play audio source
-        if (wallSource != null)
-        {
-            wallSource.Play();
-        }
-    }
-
     private void OnDisable()
     {
         if (wall != null)
@@ -56,5 +78,48 @@ public class XRAudioManager : MonoBehaviour
         }
     }
 
+    private void OnSelectEnteredGrabbable(SelectEnterEventArgs arg0)
+    {
+        if (arg0.interactableObject.transform.CompareTag("Key"))
+        {
+            grabSound.clip = keyClip;
+        }
+        else
+        {
+            grabSound.clip = grabClip;
+        }
 
+        grabSound.Play();
+    }
+
+    private void OnSelectExitedGrabbable(SelectExitEventArgs arg0)
+    {
+        grabSound.clip = grabClip;
+        grabSound.Play();
+    }
+
+    private void OnActivatedGrabbable(ActivateEventArgs arg0)
+    {
+        GameObject tempGameObj = arg0.interactableObject.transform.gameObject;
+
+        if (tempGameObj.GetComponent<WandControl>() != null)
+        {
+            activatedSound.clip = wandActivatedClip;
+        }
+        else
+        {
+            activatedSound.clip = grabActivatedClip;
+        }
+
+        activatedSound.Play();
+    }
+
+    private void OnDestroyWall()
+    {
+        //Play audio source
+        if (wallSource != null)
+        {
+            wallSource.Play();
+        }
+    }
 }
